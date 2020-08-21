c6878ece16289134a5f54d73bd9f93b41ebe3c64
Alicia Simon <asimon3@xula.edu>
2020-08-21 17:40:49 +0000

    Added user interaction

--- Sandbox.java
+++ Sandbox.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class Sandbox {
+    static Scanner scan = new Scanner(System.in);
     public static void main(String [] args){
         System.out.println("Hello, world");
+        
+        // Program that asks for user's name
+        System.out.print("What is your name? ");
+        String name = scan.nextLine();
+        System.out.printf("Hello %s\n",name);
     }
 }
